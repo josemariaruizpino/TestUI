ab43016a2b0a6fa089f41221b298f3773f2d9b45
Jose María Ruíz <josemaria.ruiz@MacBook-Pro-de-MACBP009.local>
2026-09-22 11:57:02 +0200

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,7 +33,9 @@ import { FontsDocsComponent } from './components/fonts-docs/fonts-docs.component
         DisplayDocsComponent,
         FontsDocsComponent
     ],
-    providers: [],
+    providers: [
+        { provide: LocationStrategy, useClass: HashLocationStrategy }
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
